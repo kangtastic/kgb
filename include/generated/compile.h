dea78f6c6b7e222b5d8f0c167f75b2197abe0f1b
kangtastic <kangscinate@gmail.com>
2011-10-25 23:36:02 -0700

    SCH-I500 EH09 kernel source tree from Samsung OSRC

---
+++ include/generated/compile.h
@@ -0,0 +1,9 @@
+/* This file is auto generated, version 2 */
+/*  PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#2 PREEMPT Wed Aug 31 14:16:16 KST 2011"
+#define LINUX_COMPILE_TIME "14:16:16"
+#define LINUX_COMPILE_BY "k.sridhar"
+#define LINUX_COMPILE_HOST "sep-33"
+#define LINUX_COMPILE_DOMAIN "(none)"
+#define LINUX_COMPILER "gcc version 4.4.1 (Sourcery G++ Lite 2009q3-67) "
