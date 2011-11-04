753c841b5d62d46263036b5f820bbfbe4def930d
kangtastic <kangscinate@gmail.com>
2011-11-04 05:58:13 -0700

    sched, autogroup: More fixes for autogroup so that it actually works

--- kernel/sched.c
+++ kernel/sched.c
@@ -8140,16 +8140,13 @@ void sched_move_task(struct task_struct *tsk)
 		tsk->sched_class->put_prev_task(rq, tsk);
 
 #ifdef CONFIG_FAIR_GROUP_SCHED
-	if (tsk->sched_class->prep_move_group)
-		tsk->sched_class->prep_move_group(tsk, on_rq);
+	if (tsk->sched_class->task_move_group)
+		tsk->sched_class->task_move_group(tsk, on_rq);
+	else
 #endif
 
 	set_task_rq(tsk, task_cpu(tsk));
 
-#ifdef CONFIG_FAIR_GROUP_SCHED
-	if (tsk->sched_class->moved_group)
-		tsk->sched_class->moved_group(tsk, on_rq);
-#endif
 
 	if (unlikely(running))
 		tsk->sched_class->set_curr_task(rq);
