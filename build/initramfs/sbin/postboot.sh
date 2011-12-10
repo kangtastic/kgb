e8404cb33b88bec848dfbc9c3a9d74e2da269807
kangtastic <kangscinate@gmail.com>
2011-12-09 22:37:12 -0800

    readahead: Set readahead back to 128, seems to be causing slow boots after clearing dalvik cache

--- build/initramfs/sbin/postboot.sh
+++ build/initramfs/sbin/postboot.sh
@@ -7,11 +7,12 @@
 
 # Reset internal storage readahead; may have been changed by some dumbfuck's init.d script
 # 179:0 corresponds to mmcblk0. On SCH-I500, this is eMMC (/data etc.) and NOT /sdcard
-echo 512 > /sys/devices/virtual/bdi/179:0/read_ahead_kb
+echo 128 > /sys/devices/virtual/bdi/179:0/read_ahead_kb
 
 # Set sdcard readahead on the correct device
 # 4096 may be too high latency. 1024 is next fastest value, even better than 2048 or 3072
-echo 1024 > /sys/devices/virtual/bdi/179:8/read_ahead_kb
+SDRA=/sys/devices/virtual/bdi/179:8/read_ahead_kb
+[ $(cat $SDRA) -eq 128 ] && echo 1024 > $SDRA
 
 # No need to include boot animation killer hack if the boot animation is named playlogos1
 # or samsungani because the ROM kills either automatically
