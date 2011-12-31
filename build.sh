de868373d1b05b5c75b88e0f681e581848305893
kangtastic <kangscinate@gmail.com>
2011-12-30 22:39:53 -0800

    Make it possible to choose to build for EH09, EI20 or EH03, with or without djp952's trickle charge patch
    Yes, making a whole bunch of different defconfigs may be dumb, but whatever
    The zip installer's updater-script and the build scripts get updated too

--- build.sh
+++ build.sh
@@ -8,34 +8,52 @@ START_TIME_SEC=$(date +%s)
 set -u
 set -e
 
-###############
-# DEFINITIONS #
-###############
+############################
+# VARIABLE INITIALIZATIONS #
+############################
 # Toolchain paths
 TOOLCHAIN=/home/vb/toolchain/arm-2011.03/bin
 TOOLCHAIN_PREFIX=arm-none-linux-gnueabi-
 STRIP=${TOOLCHAIN}/${TOOLCHAIN_PREFIX}strip
 
-# Kernel version tag
-KERNEL_VERSION="KGB Kernel for Samsung SCH-I500. Buildcode: $DATE.$TIME"
-
 # Other paths
 ROOTDIR=`pwd`
-BUILDDIR=$ROOTDIR/build
-WORKDIR=$BUILDDIR/bin
-OUTDIR=$BUILDDIR/out
+BUILDDIR=${ROOTDIR}/build
+WORKDIR=${BUILDDIR}/bin
+OUTDIR=${BUILDDIR}/out
 
-KERNEL_IMAGE=$ROOTDIR/arch/arm/boot/zImage
+KERNEL_IMAGE=${ROOTDIR}/arch/arm/boot/zImage
+
+# More initializations (they need to go somewhere because of set -u)
+KERNEL_VERSION=""
+TARGET=""
+TRICKLE=0
+DEFCONFIG=""
 
 ####################
 # HELPER FUNCTIONS #
 ####################
 echo_msg()
-# $1: Message to print to output
+# $1: Message, with some formatting, to print to output
 {
 echo "
 *** $1 ***
 "
+}
+help_msg()
+# Print usage information and exit
+# $1: exit code (0 or 1)
+{
+echo 'Usage: ./build.sh -t TARGET [-bh]
+
+	-t TARGET	mandatory argument to specify a ROM target
+			valid values of TARGET are "EH03", "EI20", and "EH09"
+	-b		include battery trickle charge patch by djp952 (EXPERIMENTAL)
+	-h		displays this help message and exit
+
+Example: ./build.sh -t EH03
+	 ./build.sh -t EH09 -b'
+exit $1
 }
 makezip()
 # $1: Name of output file without extension
@@ -43,7 +61,7 @@ makezip()
 {
 echo "Creating: $OUTDIR/$1.zip"
 pushd $WORKDIR/update-zip/META-INF/com/google/android > /dev/null
-sed s_"\$DATE"_"$DATE"_ < updater-script > updater-script.tmp
+sed s_"\$CODE"_"$KERNEL_VERSION"_ < updater-script > updater-script.tmp
 mv -f updater-script.tmp updater-script
 popd > /dev/null
 pushd $WORKDIR/update-zip
@@ -66,8 +84,46 @@ popd
 ####################
 # SCRIPT MAIN BODY #
 ####################
-echo "Build script run on $(date -R)"
+echo "Build script running on $(date -R)"
 
+# Parse the command line
+[ $# -eq 0 ] && echo "./build.sh: no arguments to script" >&2 && help_msg 1
+while getopts tb:h OPT; do
+	case "$OPT" in
+		t)
+			TARGET=$OPTARG
+			;;
+		b)
+			TRICKLE=1
+			;;
+		h)
+			help_msg 0
+			;;
+		\?)
+			help_msg 1
+			;;
+	esac
+done
+# Check for valid target while generating defconfig filename and kernel version string
+if [ "$TARGET" = "EH03" ]; then
+	DEFCONFIG="kgb"
+elif [ "$TARGET" = "EI20" ]; then
+	DEFCONFIG="kgb_ei20"
+elif [ "$TARGET" = "EH09" ]; then
+	DEFCONFIG="kgb_eh09"
+else
+	[ $TARGET = "" ] && TARGET="not specified"
+	echo "./build.sh: invalid target: $TARGET" >&2 && help_msg 1
+fi
+
+if [ $TRICKLE = 1 ]; then
+	DEFCONFIG="${DEFCONFIG}_trickle"
+	TARGET="${TARGET}-TRICKLE"
+fi
+KERNEL_VERSION="KGB-${TARGET}-${DATE}.${TIME}"
+DEFCONFIG="${DEFCONFIG}_defconfig"
+
+# We're starting!
 echo_msg "BUILD START: $KERNEL_VERSION"
 
 # Clean kernel and old files
@@ -78,7 +134,7 @@ make CROSS_COMPILE=$TOOLCHAIN/$TOOLCHAIN_PREFIX clean mrproper
 
 # Generate config
 echo_msg "CONFIGURING KERNEL"
-make ARCH=arm CROSS_COMPILE=$TOOLCHAIN/$TOOLCHAIN_PREFIX kgb_defconfig
+make ARCH=arm CROSS_COMPILE=$TOOLCHAIN/$TOOLCHAIN_PREFIX $DEFCONFIG
 
 # Generate initramfs
 echo_msg "GENERATING INITRAMFS"
@@ -94,10 +150,6 @@ do
 	cp -f ${line:7} $WORKDIR/initramfs/lib/modules/
 	$STRIP --strip-debug $WORKDIR/initramfs/lib/modules/$(basename $line)
 done
-
-# Replace source-built OneNAND driver with stock modules from EH03
-# cp -f $BUILDDIR/initramfs-EH03/lib/modules/dpram_atlas.ko $WORKDIR/initramfs/lib/modules/dpram_atlas.ko
-# cp -f $BUILDDIR/initramfs-EH03/lib/modules/dpram_recovery.ko $WORKDIR/initramfs/lib/modules/dpram_recovery.ko
 
 # Remove unwanted initramfs files
 rm -f $WORKDIR/initramfs/lib/modules/hotspot_event_monitoring.ko
@@ -118,14 +170,14 @@ cp -f $KERNEL_IMAGE $WORKDIR/update-zip/kernel_update/zImage
 cp -f $KERNEL_IMAGE $WORKDIR/zImage
 
 mkdir -p $OUTDIR
-makezip "KGB-I500-$DATE.$TIME"
-makeodin "KGB-I500-$DATE.$TIME"
+makezip $KERNEL_VERSION
+makeodin $KERNEL_VERSION
 
 # If you are not me, this ain't here kthx >;]
 if [ -d /mnt/vbs ]; then
-	cp -f "$OUTDIR/KGB-I500-$DATE.$TIME.tar.md5" /mnt/vbs/
-	cp -f "$OUTDIR/KGB-I500-$DATE.$TIME.zip" /mnt/vbs/
-	makeodin "KGB-I500-$DATE"
+	cp -f "${OUTDIR}/${KERNEL_VERSION}.tar.md5" /mnt/vbs/
+	cp -f "${OUTDIR}/${KERNEL_VERSION}.zip" /mnt/vbs/
+	makeodin "KGB-$(TARGET}-${DATE}"
 fi
 
 #######
--- build_log.sh
+++ build_log.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 DATE=$(date +%m%d)
 TIME=$(date +%H%M)
-bash build.sh 2>&1 | tee KGB-I500-"$DATE.$TIME".buildlog.txt
+# Run build.sh and pass any options on through
+# Save STDERR separately from combined STDOUT+STDERR
+LOG_ALL="KGB-$DATE.$TIME.buildlog.txt"
+LOG_ERR="KGB-$DATE.$TIME.builderrlog.txt"
+((./build.sh "$@" 2>&1 1>&3 | tee $LOG_ERR) 3>&1 1>&2) 2>&1 | tee $LOG_ALL
 echo ""
-echo "Log saved as KGB-I500-"$DATE.$TIME".buildlog.txt"
+echo "Logs saved as $LOG_ALL and $LOG_ERR"
