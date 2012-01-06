ea9a7b8429df035a4256407c5e99c9a87ddcd849
kangtastic <kangscinate@gmail.com>
2012-01-05 23:53:00 -0800

    fb: Don't use Voodoo Control to tune color (pawitp)

--- drivers/video/samsung/s3cfb_tl2796.c
+++ drivers/video/samsung/s3cfb_tl2796.c
@@ -448,16 +448,6 @@ void tl2796_late_resume(struct early_suspend *h)
 	return ;
 }
 
-static ssize_t gamma_table_show(struct device *dev, struct device_attribute *attr, char *buf)
-{
-	return sprintf(buf, "<unsupported>\n");
-}
-
-static ssize_t gamma_table_store(struct device *dev, struct device_attribute *attr, const char *buf, size_t size)
-{
-	return size;
-}
-
 static ssize_t red_multiplier_show(struct device *dev, struct device_attribute *attr, char *buf)
 {
 	return sprintf(buf, "%u\n", color_mult[0]);
@@ -506,36 +496,14 @@ static ssize_t blue_multiplier_store(struct device *dev, struct device_attribute
 	return size;
 }
 
-// All multipliers default to U32_MAX
-static ssize_t multiplier_original_show(struct device *dev, struct device_attribute *attr, char *buf)
-{
-	return sprintf(buf, "%u\n", U32_MAX);
-}
-
-static ssize_t color_tuning_version_show(struct device *dev, struct device_attribute *attr, char *buf)
-{
-	return sprintf(buf, "CM7 SGS Color Tuning\n");
-}
-
-// We don't support gamma_table replacement, but Voodoo Control App will crash on boot without it
-static DEVICE_ATTR(gamma_table, S_IRUGO | S_IWUGO, gamma_table_show, gamma_table_store);
 static DEVICE_ATTR(red_multiplier, S_IRUGO | S_IWUGO, red_multiplier_show, red_multiplier_store);
-static DEVICE_ATTR(red_multiplier_original, S_IRUGO, multiplier_original_show, NULL);
 static DEVICE_ATTR(green_multiplier, S_IRUGO | S_IWUGO, green_multiplier_show, green_multiplier_store);
-static DEVICE_ATTR(green_multiplier_original, S_IRUGO, multiplier_original_show, NULL);
 static DEVICE_ATTR(blue_multiplier, S_IRUGO | S_IWUGO, blue_multiplier_show, blue_multiplier_store);
-static DEVICE_ATTR(blue_multiplier_original, S_IRUGO, multiplier_original_show, NULL);
-static DEVICE_ATTR(version, S_IRUGO, color_tuning_version_show, NULL);
 
 static struct attribute *color_tuning_attributes[] = {
-	&dev_attr_gamma_table.attr,
 	&dev_attr_red_multiplier.attr,
-	&dev_attr_red_multiplier_original.attr,
 	&dev_attr_green_multiplier.attr,
-	&dev_attr_green_multiplier_original.attr,
 	&dev_attr_blue_multiplier.attr,
-	&dev_attr_blue_multiplier_original.attr,
-	&dev_attr_version.attr,
 	NULL
 };
 
@@ -545,7 +513,7 @@ static struct attribute_group color_tuning_group = {
 
 static struct miscdevice color_tuning_device = {
 	.minor = MISC_DYNAMIC_MINOR,
-	.name = "voodoo_color",
+	.name = "color_tuning",
 };
 
 static int __devinit tl2796_probe(struct spi_device *spi)
